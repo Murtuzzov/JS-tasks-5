06a52ceb4946a9c6e2896c5519bcf3d83666eb3d
murtuzzov <murtuzzov>
2024-02-24 17:15:40 +0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,44 @@
+//ЗАДАЧА No1
+// let start = parseInt(prompt("Введите начальное число диапазона:"));
+// let end = parseInt(prompt("Введите конечное число диапазона:"));
+// let sum = 0;
+
+// for (let i = start; i <= end; i += 1) {
+//   sum += i;
+// }
+
+// alert(`Сумма чисел в заданном диапазоне от ${start} до ${end} равна ${sum}`);
+
+//ЗАДАЧА No2
+// let numFirst = parseInt(prompt("Введите первое число:"));
+// let numSecond = parseInt(prompt("Введите второе число:"));
+// let nod;
+
+// while (numFirst !== numSecond) {
+//   if (numFirst > numSecond) {
+//     numFirst -= numSecond;
+//   } else {
+//     numSecond -= numFirst;
+//   }
+// }
+// nod = numFirst;
+// alert("НОД чисел равен " + nod);
+
+//ЗАДАЧА No3
+// const number = parseInt(prompt("Введите число:"));
+// alert(`Количество цифр в числе ${number} равно ${quantity}`);
+// for (let i = 1; i <= number; i += 1) {
+//   if (number % i === 0) {
+//     alert(i);
+//   }
+// }
+
+//ЗАДАЧА No4
+// const number = prompt("Введите число:");
+// let quantity = 0;
+
+// for (let i = 0; i < number.length; i += 1) {
+//   quantity += 1;
+// }
+
+// alert(`Количество цифр в числе ${number} равно ${quantity}`);
